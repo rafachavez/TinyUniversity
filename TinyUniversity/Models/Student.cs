72d1514fd705b5ad0f48155280a6f83066d6a048
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-06 02:03:36 -0600

    added models to folder: course, enrollment and student

---
+++ TinyUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstMidName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //Navegation Properties
+        public ICollection<Enrollment> Enrollments {get; set;}
+
+    }
+}
