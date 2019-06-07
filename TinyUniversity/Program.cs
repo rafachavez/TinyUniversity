fa3336cd39e06ea2b57efc3fc10cb671eb25b75c
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-06 19:50:37 -0600

    updated program.cs to inject identity framework

--- TinyUniversity/Program.cs
+++ TinyUniversity/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using TinyUniversity.Models;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace TinyUniversity
 {
@@ -14,7 +17,25 @@ namespace TinyUniversity
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
