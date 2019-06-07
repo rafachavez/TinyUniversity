010fb20efb68d6e4cb9495b9879443c546730e20
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-06 18:59:02 -0600

    generated students folder with files  to work on the students class and created file to help connection to database

---
+++ TinyUniversity/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TinyUniversity.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TinyUniversity.Models.Student> Student { get; set; }
+    }
+}
